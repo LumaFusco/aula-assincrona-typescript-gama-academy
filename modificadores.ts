c5ca468b085d10ac47b8c5c11165a5a7e3240121
LumaFusco <lumafuscoconde@gmail.com>
2023-01-16 21:31:05 -0300

    sobre modificadores

--- modificadores.ts
+++ modificadores.ts
@@ -1,13 +1,55 @@
 // utilizando MODIFICADORES DE ACESSO
 
 class Carro {
-    velocidadeAtual: number = 0;
+    private velocidadeAtual: number = 0;
 
     constructor(
         public marca: string,
         public modelo: string,
-        private velocidadeMaxima: number = 220
+        private velocidadeMaxima: number = 220  //private praNinguemMudar, sóDentroDaClasse
     ) { }
+
+    //para alterar dentro da própria classe no caso de private:
+
+    private alterarVelocidade(delta: number) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+
+        if (novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+    }
+
+    acelerar() {
+        this.alterarVelocidade(5);
+    }
+
+    frear() {
+        this.alterarVelocidade(-5);
+    }
+
 }
 
-const carro = new Carro('Chevrolet', 'Prisma', 250);
+const carro = new Carro ("Nivus", "Volks", 250);
+carro.acelerar();
+
+
+//HERANÇA
+class Camaro extends Carro{                         //extends pra criar herança de Carro
+    private turbo = false;
+    
+    constructor(){
+        super("Chevrolet", "Camaro", 500);         //super para chamar a classe pai
+    }
+
+    ligarTurbo(){
+        this.turbo = true;
+    }
+}
+
+const camaro = new Camaro();
+camaro.acelerar()
+camaro.frear();
+camaro.ligarTurbo();
